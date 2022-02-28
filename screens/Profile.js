c5ceede64612134d41235eb1de89d1a5d0df21db
Umer Rehan <umerrehan@Umers-MacBook-Air.local>
2022-03-01 00:20:21 +0500

    Profile Button

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View,Image, } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import { Card, Title } from 'react-native-paper';
-import { MaterialIcons } from '@expo/vector-icons';
+import { Card, Title ,Button} from 'react-native-paper';
+import { MaterialIcons,Entypo,Feather } from '@expo/vector-icons';
 
 export default function Profile() {
   return (
@@ -29,10 +29,36 @@ style={styles.img}
     <View style={styles.mainview}>
 
       <Card style={styles.card}>
-        <MaterialIcons name="email" size={20} color="black" />
-        <Text>abc@gmail.com</Text>
+        <View style={styles.cardview}>
+          <MaterialIcons name="email" size={20} color="black" />
+          <Text style={{paddingLeft:20}}>abc@gmail.com</Text>
+        </View>
       </Card>
-    
+
+      <Card style={styles.card}>
+        <View style={styles.cardview}>
+        <Entypo name="location-pin" size={20} color="black" />
+          <Text style={{paddingLeft:20}}>Tariq Road</Text>
+        </View>
+      </Card>
+
+      <Card style={styles.card}>
+        <View style={styles.cardview}>
+        <Entypo name="phone" size={20} color="black" />
+          <Text style={{paddingLeft:20}}>+92 123-9090881</Text>
+        </View>
+        </Card>
+
+        <View style={styles.btn}>
+          <Button color='#009ea1' icon="account-edit" mode="contained" onPress={() => console.log('Pressed')}>
+            Edit Profile
+          </Button>
+
+          <Button color='#009ea1' icon="delete-sweep-outline" mode="contained" onPress={() => console.log('Pressed')}>
+            Delete Profile
+          </Button>
+        </View>
+
     </View>
   
   </View>
@@ -54,12 +80,21 @@ const styles = StyleSheet.create({
     borderRadius:170/2,
   },
   mainview:{
-    marginTop:50,
+    marginTop:40,
     margin:10,
   },
   card:{
+    borderRadius:10,
+    padding:10,
+    marginBottom:15,
+  },
+  cardview:{
     flexDirection:'row',
-  }
+  },
+  btn:{
+    flexDirection:'row',
+    justifyContent:'space-evenly',
+  },
 
 
 });
